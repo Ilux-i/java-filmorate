adcdfae37e33e1d6d9eac41f1ee51d7674b0e9be
Ilux-i <iluxeo6@gmail.com>
2025-12-17 19:32:22 +0300

    feature
    Обновлены сущность User и Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -15,7 +15,28 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
+    private Rating rating;
 
     @Builder.Default
     private Set<Long> likes = new HashSet<>();
+
+    @Builder.Default
+    private Set<Genre> genres = new HashSet<>();
+}
+
+enum Rating {
+    G,
+    PG,
+    PG_13,
+    R,
+    NC_17
+}
+
+enum Genre {
+    Comedy,
+    Drama,
+    Animation,
+    Thriller,
+    Documentary,
+    Action
 }
