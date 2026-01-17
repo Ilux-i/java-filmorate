767d5eba6923a6118c430d9b29ef893b4cba1cf0
Ilux-i <iluxeo6@gmail.com>
2026-01-17 15:59:48 +0300

    feature
    Реализована вся логика согласно тз.
    Все тесты проходит.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/DirectorRepository.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.dao.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public class DirectorRepository extends BaseRepository<Director> {
+
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM directors WHERE id = ?";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM directors order by id";
+    private static final String INSERT_QUERY = "INSERT INTO directors(name) VALUES (?)";
+    private static final String UPDATE_QUERY = "UPDATE directors SET name = ? WHERE id = ?";
+    private static final String DELETE_QUERY = "DELETE FROM directors WHERE id = ?";
+
+    public DirectorRepository(JdbcTemplate jdbc, RowMapper<Director> mapper) {
+        super(jdbc, mapper);
+    }
+
+    // Получение режиссёра по id
+    public Optional<Director> findById(long directorId) {
+        return findOne(FIND_BY_ID_QUERY, directorId);
+    }
+
+    // Получение списка всех режиссёров
+    public List<Director> findAll() {
+        return findMany(FIND_ALL_QUERY);
+    }
+
+    // Добавление режиссёра
+    public Director add(Director director) {
+        long id = insert(
+                INSERT_QUERY,
+                director.getName()
+        );
+        director.setId(id);
+        return director;
+    }
+
+    // Обновление режиссёра
+    public Director update(Director director) {
+        update(
+                UPDATE_QUERY,
+                director.getName(),
+                director.getId()
+        );
+        return director;
+    }
+
+    // Удаление режиссёра по id
+    public boolean remove(long directorId) {
+        return delete(DELETE_QUERY, directorId);
+    }
+
+    public Set<Director> findAllByList(List<Long> list) {
+        return null;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/FilmDirectorRepository.java
@@ -0,0 +1,81 @@
+package ru.yandex.practicum.filmorate.dao.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dto.film_director.FilmDirectorDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class FilmDirectorRepository extends BaseRepository<FilmDirectorDto> {
+
+    private static final String FIND_ALL_DIRECTORS_QUERY = "SELECT * FROM film_director WHERE film_id = ? order by director_id";
+    private static final String FIND_ALL_FILMS_QUERY = "SELECT * FROM film_director WHERE director_id = ? order by film_id";
+    private static final String INSERT_QUERY = "INSERT INTO film_director(film_id, director_id) VALUES (?, ?)";
+    private static final String INSERT_DIRECTORS_IN_FILM_QUERY = "INSERT INTO film_director(film_id, director_id) VALUES (?, ?)";
+    private static final String REMOVE_FILM_DIRECTOR_QUERY = "DELETE FROM film_director WHERE film_id = ? AND director_id = ?";
+    private static final String DELETE_BY_LIST_QUERY = "DELETE FROM film_director WHERE ";
+
+    public FilmDirectorRepository(JdbcTemplate jdbc, RowMapper<FilmDirectorDto> mapper) {
+        super(jdbc, mapper);
+    }
+
+    // Получение списка FilmDirectorDto в порядке возрастания directorId
+    public List<FilmDirectorDto> findAllByFilm(long filmId) {
+        return findMany(FIND_ALL_DIRECTORS_QUERY, filmId);
+    }
+
+    // Получение списка FilmDirectorDto в порядке возрастания filmId
+    public List<FilmDirectorDto> findAllByDirector(long directorId) {
+        return findMany(FIND_ALL_FILMS_QUERY, directorId);
+    }
+
+    // Добавление связи фильм-режиссёр
+    public FilmDirectorDto add(FilmDirectorDto dto) {
+        long id = insert(
+                INSERT_QUERY,
+                dto.getFilmId(),
+                dto.getDirectorId()
+        );
+        dto.setId(id);
+        return dto;
+    }
+
+    // Добавление списка режиссёров к фильму по id
+    public void addDirectorsToFilm(long filmId, List<Long> directorIds) {
+        if (directorIds == null || directorIds.isEmpty()) {
+            return;
+        }
+
+        // Подготовка данных к сохранению в бд
+        List<Object[]> batchArgs = directorIds.stream()
+                .map(directorId -> new Object[]{filmId, directorId})
+                .collect(Collectors.toList());
+
+        // Сохранение данных
+        jdbc.batchUpdate(INSERT_DIRECTORS_IN_FILM_QUERY, batchArgs);
+    }
+
+    // Удаление связи фильм-режиссёр
+    public boolean remove(long filmId, long directorId) {
+        return jdbc.update(REMOVE_FILM_DIRECTOR_QUERY, filmId, directorId) > 0;
+    }
+
+    // Удаление связей фильм-режиссёр
+    public void removeFilmDirectorByPair(List<FilmDirectorDto> pairs) {
+        if (pairs.isEmpty()) return;
+        List<String> conditions = new ArrayList<>();
+        List<Object> params = new ArrayList<>();
+        for (FilmDirectorDto pair : pairs) {
+            conditions.add("(film_id = ? AND director_id = ?)");
+            params.add(pair.getFilmId());
+            params.add(pair.getDirectorId());
+        }
+        String whereClause = String.join(" OR ", conditions);
+
+        update(DELETE_BY_LIST_QUERY + whereClause, params.toArray());
+    }
+}
