cd5ad5f4ef9b4f970281902674b40945fb736a7a
Ilux-i <iluxeo6@gmail.com>
2026-01-19 11:57:08 +0300

    fix
    Добавление удалённых эндпоинтов
    
    Cлитые изменения из веток:
    - add-director
    - add-remove-endpoint
    - add-common-films

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -30,6 +30,12 @@ public class FilmController {
         return filmService.updateFilm(film);
     }
 
+    // Удаление фильма
+    @DeleteMapping("/{id}")
+    public void deleteFilm(@PathVariable long id) {
+        filmService.remove(id);
+    }
+
     // Добавление лайка к фильму от пользователя по их id соответственно
     @PutMapping("/{filmId}/like/{userId}")
     public Film addLike(@PathVariable final long filmId, @PathVariable final long userId) {
@@ -64,5 +70,11 @@ public class FilmController {
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLike(@PathVariable final long id, @PathVariable final long userId) {
         filmService.removeLike(userId, id);
+    }
+
+    // Получение общих фильмов
+    @GetMapping("/common")
+    public Collection<Film> getCommonFilms(@RequestParam final long userId, @RequestParam final long friendId) {
+        return filmService.getCommonFilms(userId, friendId);
     }
 }
