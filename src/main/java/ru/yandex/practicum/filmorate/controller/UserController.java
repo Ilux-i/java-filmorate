960cd71e41a9977a72a6b435dfba993a10bc4217
Ilux-i <iluxeo6@gmail.com>
2026-01-20 22:16:03 +0300

    .

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Feed;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.service.RecommendationService; // Добавьте этот импорт
-import ru.yandex.practicum.filmorate.model.Film; // Добавьте этот импорт
+import ru.yandex.practicum.filmorate.service.RecommendationService;
+import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
-import java.util.List; // Добавьте этот импорт
 
 @Slf4j
 @RestController()
