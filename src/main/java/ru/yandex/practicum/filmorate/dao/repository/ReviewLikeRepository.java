efce7f133f33a1bbbf8a903613da981d087b8aa3
Ilux-i <iluxeo6@gmail.com>
2026-01-23 19:00:21 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/ReviewLikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/ReviewLikeRepository.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import java.util.Optional;
